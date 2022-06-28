16d57f29acc23f2d40371eb26632b0ae41b95e56
Sererum <sererum.ab@yandex.ru>
2022-06-28 19:29:01 +0400

    Add project files.

---
+++ Organizer/Internal/Activity/MainActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+
+namespace Organizer.Internal.Activity
+{
+    [Activity(Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate (Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
